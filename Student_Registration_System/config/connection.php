73238449bebb6a24d006501686675f0a99f6e15d
Joish T Joshy <58565210+JoishJoshy@users.noreply.github.com>
2020-01-17 18:38:06 +0530

    Add files via upload

---
+++ Student_Registration_System/config/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+$host       = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "student_system"; 
+
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
